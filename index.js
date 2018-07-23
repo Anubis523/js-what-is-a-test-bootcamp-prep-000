8c549a9da20077bef45d390d29a7124537c445dc
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 10:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
-https://github.com/learn-co-students/js-what-is-a-test-bootcamp-prep-000
